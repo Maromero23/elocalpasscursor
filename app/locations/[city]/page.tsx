ae9885a781e5b49ecfe1fd0b03b785df37f038f8
Maromero23 <jorgeruiz23@gmail.com>
2025-07-17 21:28:47 -0500

    Update affiliate cards - remove phone/email icons, add Facebook/Instagram icons, remove address, add description and prominent discount display

--- app/locations/[city]/page.tsx
+++ app/locations/[city]/page.tsx
@@ -535,21 +535,36 @@ export default function CityPage() {
                         </div>
                       </div>
 
-                      <div className="flex items-center text-sm text-gray-500 mb-2">
-                        <MapPin className="w-4 h-4 mr-1" />
-                        <span className="truncate">{affiliate.address}</span>
-                      </div>
+                      {/* Description */}
+                      {affiliate.description && (
+                        <div className="text-sm text-gray-600 mb-3 line-clamp-2">
+                          {affiliate.description}
+                        </div>
+                      )}
+
+                      {/* Discount - Most Important */}
+                      {affiliate.discount && (
+                        <div className="mb-3">
+                          <div className="text-lg font-bold text-orange-600">
+                            {affiliate.discount}
+                          </div>
+                        </div>
+                      )}
 
                       <div className="flex items-center justify-between">
                         <div className="flex items-center space-x-2">
-                          {affiliate.workPhone && (
-                            <Phone className="w-4 h-4 text-gray-400" />
-                          )}
-                          {affiliate.email && (
-                            <Mail className="w-4 h-4 text-gray-400" />
-                          )}
                           {affiliate.web && (
                             <Globe className="w-4 h-4 text-gray-400" />
+                          )}
+                          {affiliate.facebook && (
+                            <svg className="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 24 24">
+                              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                            </svg>
+                          )}
+                          {affiliate.instagram && (
+                            <svg className="w-4 h-4 text-gray-400" fill="currentColor" viewBox="0 0 24 24">
+                              <path d="M12.017 0C5.396 0 .029 5.367.029 11.987c0 6.62 5.367 11.987 11.988 11.987 6.62 0 11.987-5.367 11.987-11.987C24.014 5.367 18.637.001 12.017.001zM8.449 16.988c-1.297 0-2.448-.49-3.323-1.297C4.198 14.895 3.708 13.744 3.708 12.447s.49-2.448 1.418-3.323c.875-.807 2.026-1.297 3.323-1.297s2.448.49 3.323 1.297c.928.875 1.418 2.026 1.418 3.323s-.49 2.448-1.418 3.244c-.875.807-2.026 1.297-3.323 1.297zm7.718-1.297c-.875.807-2.026 1.297-3.323 1.297s-2.448-.49-3.323-1.297c-.928-.875-1.418-2.026-1.418-3.244s.49-2.448 1.418-3.323c.875-.807 2.026-1.297 3.323-1.297s2.448.49 3.323 1.297c.928.875 1.418 2.026 1.418 3.323s-.49 2.448-1.418 3.244z"/>
+                            </svg>
                           )}
                         </div>
                         <div className="flex items-center space-x-1">
